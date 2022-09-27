e85654d1f9b09ccfe7327e7e9bc4f1fcd091c9b4
fikri <fikri.aslam102519@students.unila.ac.id>
2022-09-27 15:41:41 +0700

    fix bug

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 
 hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
     menu.classList.toggle('hidden');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   Content : [ 'index.html'],
   theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+
+    },
     extend: {},
   },
   plugins: [],
